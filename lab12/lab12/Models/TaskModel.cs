4428c8d575398b2ba9c3ab926bc7cc6f4f89d646
johanrmy <95696635+johanrmy@users.noreply.github.com>
2023-12-02 12:48:37 -0500

    Update dataType isCompleted

--- lab12/lab12/Models/TaskModel.cs
+++ lab12/lab12/Models/TaskModel.cs
@@ -9,7 +9,7 @@ namespace lab12.Models
         public int Id { get; set; }
         public String title { get; set; }
         public String description { get; set; }
-        public bool isCompleted { get; set; }
+        public string isCompleted { get; set; }
 
         public TaskModel() { }
 
